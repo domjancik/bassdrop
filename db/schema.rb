43c75280811521c862290ed08b1d35ad34e1d8a5
Dominik Jancik <jancidom@fit.cvut.cz>
2015-04-04 17:57:54 +0200

    Renamed new social columns for artists so they are more consistent with link_fb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150404105136) do
+ActiveRecord::Schema.define(version: 20150404155600) do
 
   create_table "artists", force: :cascade do |t|
     t.string   "title",               limit: 255
@@ -24,9 +24,9 @@ ActiveRecord::Schema.define(version: 20150404105136) do
     t.string   "image_url_cached",    limit: 255
     t.integer  "role",                limit: 4,        default: 0
     t.integer  "playlist_id",         limit: 4
-    t.string   "twitter_link",        limit: 255
-    t.string   "youtube_link",        limit: 255
-    t.string   "instagram_link",      limit: 255
+    t.string   "link_twitter",        limit: 255
+    t.string   "link_youtube",        limit: 255
+    t.string   "link_instagram",      limit: 255
     t.text     "description",         limit: 16777215
   end
 
