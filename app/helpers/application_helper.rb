91f99c60913f00d6711f0c9febcb70c1a7b39baa
Dominik Jancik <jancidom@fit.cvut.cz>
2015-04-07 13:25:09 +0200

    Better link processing

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def desc_format(data)
+    auto_link(simple_format(data)) do |url|
+      new_url = url
+      new_url.gsub! /https?:\/\//, ''
+      new_url.truncate 40
+    end
+  end
 end
