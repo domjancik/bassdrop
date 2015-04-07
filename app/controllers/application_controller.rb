5542a2e3697ad26ccf311b313ccb709dc17a32b9
Dominik Jancik <jancidom@fit.cvut.cz>
2015-04-07 15:43:32 +0200

    Admin check in views, admin buttons style

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   helper NavigationHelper
   helper ApplicationHelper
+  helper UsersHelper
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
