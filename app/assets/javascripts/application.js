61906d1bf639eb412940eb0d82189ef672f43c7c
Dominik Jancik <jancidom@fit.cvut.cz>
2015-04-10 14:18:09 +0200

    Safari fix?

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -52,16 +52,16 @@ var init_loading_links = function() {
     });
 }
 
-var is_chrome = navigator.userAgent.indexOf('Chrome') > -1;
-var is_explorer = navigator.userAgent.indexOf('MSIE') > -1;
-var is_firefox = navigator.userAgent.indexOf('Firefox') > -1;
-var is_safari = navigator.userAgent.indexOf("Safari") > -1;
-var is_opera = navigator.userAgent.toLowerCase().indexOf("op") > -1;
-if ((is_chrome)&&(is_safari)) {is_safari=false;}
-if ((is_chrome)&&(is_opera)) {is_chrome=false;}
+//var is_chrome = navigator.userAgent.indexOf('Chrome') > -1;
+//var is_explorer = navigator.userAgent.indexOf('MSIE') > -1;
+//var is_firefox = navigator.userAgent.indexOf('Firefox') > -1;
+//var is_safari = navigator.userAgent.indexOf("Safari") > -1;
+//var is_opera = navigator.userAgent.toLowerCase().indexOf("op") > -1;
+//if ((is_chrome)&&(is_safari)) {is_safari=false;}
+//if ((is_chrome)&&(is_opera)) {is_chrome=false;}
 
 var init_avatar_carousels = function() {
-    if (is_safari) return;
+    //if (is_safari) return;
     var avatar_links = $('.avatar-carousel');
     if (avatar_links.length > 0) {
         avatar_links.each(function(index) {
