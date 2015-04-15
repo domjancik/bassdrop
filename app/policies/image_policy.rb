a568135bada7439c580e7444ee78bc9c7a79a572
jancidom <dominik.jancik@urbanevents.cz>
2015-04-16 00:50:18 +0200

    Help text for stories regarding images, link to images

--- app/policies/image_policy.rb
+++ app/policies/image_policy.rb
@@ -3,6 +3,18 @@ class ImagePolicy < ApplicationPolicy
     true
   end
 
+  def edit?
+    is_vip?
+  end
+
+  def new?
+    is_vip?
+  end
+
+  def create?
+    is_vip?
+  end
+
   class Scope < Scope
     def resolve
       scope
