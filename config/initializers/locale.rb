45e3e1041c95ad2913b6a31d031a55e82aa35163
jancidom <dominik.jancik@urbanevents.cz>
2015-04-19 23:20:54 +0200

    Trying it differently

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -2,4 +2,4 @@ require 'i18n/backend/fallbacks'
 I18n::Backend::Simple.send(:include, I18n::Backend::Fallbacks)
 
 I18n.default_locale = :cs
-# I18n.fallbacks.map(cs: :en)
+I18n.fallbacks.map(cs: :en)
